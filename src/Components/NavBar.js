3e3e124b33ed684a36855a12ffb006ce34b6616d
Ebenezhar <ebenezharselvakumar@gmail.com>
2022-12-22 14:52:49 +0530

    duplicated removed from nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
     return (
@@ -18,35 +17,6 @@ function NavBar() {
             </Container>
         </Navbar>
 
-        // <nav className="navbar navbar-expand-lg nav-style" data-aos-once="true" data-aos="fade-down" style={{ position: "sticky" }}>
-        //     <div className="container-fluid ">
-        //         {/* <Player
-        //             className='player'
-        //             background="transparent"
-        //             autoplay
-        //             loop
-        //             src="https://assets7.lottiefiles.com/packages/lf20_32v7KqVd8G.json"
-        //             style={{ height: '50px', width: '50px', background: 'transperant' }}
-        //         >
-        //             <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-        //         </Player> */}
-        //         <a className="btn navbar-brand px-md-2 profile-name fw-bold" href="/">ANIMAL SHELTER</a>
-        //         <button class="navbar-toggler custom-toggler" type="button" dat-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span class="navbar-toggler-icon"></span>
-        //         </button>
-
-        //         <div className="collapse me-3 navbar-collapse nav-head d-flex justify-content-center" id="navbarSupportedContent">
-        //             <ul className="navbar-nav d-flex justify-content-center mb-lg-0 " >
-        //                 <li className="nav-item ">
-        //                     <a className="nav-link nav-text fw-bold" href="#">Home</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link nav-text fw-bold" href="#" >Contact Us</a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
     )
 }
 
